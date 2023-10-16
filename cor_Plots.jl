16ba8ffcc92c86b1b9dc87ff52604c4939dcc53d
Shubo <sfe55@uclive.ac.nz>
2023-10-16 23:46:34 +1300

    Update julia data processing.

--- cor_Plots.jl
+++ cor_Plots.jl
@@ -1,34 +1,15 @@
-using Plots
-using CSV
-using DataFrames
 
-file_path = "D:/data422/data/correlation_result/cor_incidence.csv"
-cor_incidence = CSV.File(file_path) |> DataFrame
+using Plots, DataFrames, CSV
 
-# Display the first few rows of the DataFrame
-display(cor_incidence)
 
-# Assuming you have a DataFrame in Julia, and it's similar to the R data.frame
-# For this example, let's create a DataFrame similar to your R code
-cor_incidence = DataFrame(
-    cor = randn(100),
-    p_adjusted = rand(),
-    cancer = rand(["A", "B"], 100)
-)
+cor_incidence_path = "data/correlation_result/cor_incidence.csv"
 
-# Julia equivalent code using Plots.jl
-plot(
-    cor_incidence[:cor], 
-    -log10.(cor_incidence[:p_adjusted]),
-    seriestype=:scatter,
-    group=cor_incidence[:cancer],
-    size=1.5,
-    alpha=0.6,
-    legend=:topleft,
-    xlabel="cor",
-    ylabel="-log10(p_adjusted)",
-    color=:auto
-)
+# Load data from data_path
+cor_incidence = CSV.File(cor_incidence_path) |> DataFrame
 
-vline!([-0.2, 0.2], linestyle=:dash, linecolor=:grey, linewidth=0.8)
-hline!([2], linestyle=:dash, linecolor=:grey, linewidth=0.8)
+scatter(cor_incidence.cor, -log10.(cor_incidence.p_adjusted), group=cor_incidence.cancer, xlabel="cor", ylabel="-log(p_adjusted)", title="Coefficient and P_value in Incidence",
+markershape=:circle, markerstrokewidth=0, alpha=0.4, markerstrokealpha=0, size=(650, 400), legend=:outertopright)
+hline!([2], color=:gray, linestyle=:dash, label="y=2")  
+vline!([0.2, -0.2], color=:gray, linestyle=:dash, label="x=0.2, x=-0.2")  
+
+
